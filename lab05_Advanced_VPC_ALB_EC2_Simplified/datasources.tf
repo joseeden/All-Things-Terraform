7f33e3fe966ca0cb76735fc68ff177b41590b0da
joseeden <josemanuelitoeden@gmail.com>
2022-06-08 19:33:04 +0800

    Added new lab05

---
+++ lab05_Advanced_VPC_ALB_EC2_Simplified/datasources.tf
@@ -0,0 +1,15 @@
+
+data "aws_ami" "lab04_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
---
+++ lab05_Advanced_VPC_ALB_EC2_Simplified/variables.tf
@@ -0,0 +1,35 @@
+# Variables for setting up terraform
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "my_credentials" {
+  description = "Credentials to be used to connect to AWS"
+  type        = list(string)
+}
+
+variable "my_profile" {
+  description = "Profile to be used to connect to AWS"
+  type        = string
+}
+
+# Variables for creating the VPC and EC2 instances
+
+variable "instance_type" {
+  type = string
+}
+
+variable "avail_zones" {
+  type = list(string)
+}
+
+variable "cidr_block" {
+  type = string
+}
+
+variable "my_ip" {
+  type = string
+}
+
